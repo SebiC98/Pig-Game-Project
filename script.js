a5e2f8e168649e55e1db9cda837f34e266562163
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2021-01-07 21:36:56 +0200

    Switching the Active Player

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
@@ -9,13 +12,15 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const currentScore0El = document.querySelector('#score--0');
-const currentScore1El = document.querySelector('#score--1');
+const totalScore0El = document.querySelector('#score--0');
+const totalScore1El = document.querySelector('#score--1');
 
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 //Starting conditions
 
 score0El.textContent = 0;
@@ -36,9 +41,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add the value to the current score
     currentScore += dice;
-    currentScore0El.textContent = currentScore;
-    current0El.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
